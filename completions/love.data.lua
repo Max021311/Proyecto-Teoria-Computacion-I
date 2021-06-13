6f891dfc919ac2083578cbb1bf2872b8c2768292
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2021-06-13 00:49:03 -0500

    Fixing errors in completion

--- completions/love.data.lua
+++ completions/love.data.lua
@@ -17,7 +17,8 @@ local ByteData = {}
 local CompressedData = {}
 ---Gets the compression format of the CompressedData.
 ---@return CompressedDataFormat
-function CompressedData:getFormat() end
+function CompressedData:getFormat()
+end
 
 --endregion CompressedData
 ---Compressed data formats.
@@ -29,21 +30,21 @@ CompressedDataFormat = {
 	---The gzip format is DEFLATE-compressed data with a slightly larger header than zlib. Since it uses DEFLATE it has the same compression characteristics as the zlib format.
 	['gzip'] = 3,
 	---Raw DEFLATE-compressed data (no header).
-	['deflate'] = 4,
+	['deflate'] = 4
 }
 ---Return type of various data-returning functions.
 ContainerType = {
 	---Return type is ByteData.
 	['data'] = 1,
 	---Return type is string.
-	['string'] = 2,
+	['string'] = 2
 }
 ---Encoding format used to encode or decode data.
 EncodeFormat = {
 	---Encode/decode data as base64 binary-to-text encoding.
 	['base64'] = 1,
 	---Encode/decode data as hexadecimal string.
-	['hex'] = 2,
+	['hex'] = 2
 }
 ---Hash algorithm of love.data.hash.
 HashFunction = {
@@ -58,7 +59,7 @@ HashFunction = {
 	---SHA2 hash algorithm with message digest size of 384 bits (48 bytes).
 	['sha384'] = 5,
 	---SHA2 hash algorithm with message digest size of 512 bits (64 bytes).
-	['sha512'] = 6,
+	['sha512'] = 6
 }
 ---Compresses a string or data using a specific compression algorithm.
 ---@param container ContainerType @What type to return the compressed data as.
@@ -66,8 +67,9 @@ HashFunction = {
 ---@param rawstring string @The raw (un-compressed) string to compress.
 ---@param level number @The level of compression to use, between 0 and 9. -1 indicates the default level. The meaning of this argument depends on the compression format being used.
 ---@return CompressedData or string
----@overload fun(container:ContainerType, format:CompressedDataFormat, data:Data, level:number):CompressedData or string
-function m.compress(container, format, rawstring, level) end
+---@overload fun(container:ContainerType, format:CompressedDataFormat, data:Data, level:number):CompressedData | string
+function m.compress(container, format, rawstring, level)
+end
 
 ---Decode Data or a string from any of the EncodeFormats to Data or string.
 ---@param container ContainerType @What type to return the decoded data as.
@@ -75,7 +77,8 @@ function m.compress(container, format, rawstring, level) end
 ---@param sourceString string @The raw (encoded) data to decode.
 ---@return ByteData or string
 ---@overload fun(container:ContainerType, format:EncodeFormat, sourceData:Data):ByteData or string
-function m.decode(container, format, sourceString) end
+function m.decode(container, format, sourceString)
+end
 
 ---Decompresses a CompressedData or previously compressed string or Data object.
 ---@param container ContainerType @What type to return the decompressed data as.
@@ -83,7 +86,8 @@ function m.decode(container, format, sourceString) end
 ---@return Data or string
 ---@overload fun(container:ContainerType, format:CompressedDataFormat, compressedString:string):Data or string
 ---@overload fun(container:ContainerType, format:CompressedDataFormat, data:Data):Data or string
-function m.decompress(container, compressedData) end
+function m.decompress(container, compressedData)
+end
 
 ---Encode Data or a string to a Data or string in one of the EncodeFormats.
 ---@param container ContainerType @What type to return the encoded data as.
@@ -92,21 +96,24 @@ function m.decompress(container, compressedData) end
 ---@param linelength number @The maximum line length of the output. Only supported for base64, ignored if 0.
 ---@return ByteData or string
 ---@overload fun(container:ContainerType, format:EncodeFormat, sourceData:Data, linelength:number):ByteData or string
-function m.encode(container, format, sourceString, linelength) end
+function m.encode(container, format, sourceString, linelength)
+end
 
 ---Gets the size in bytes that a given format used with love.data.pack will use.
 ---
 ---This function behaves the same as Lua 5.3's string.packsize.
 ---@param format string @A string determining how the values are packed. Follows the rules of Lua 5.3's string.pack format strings.
 ---@return number
-function m.getPackedSize(format) end
+function m.getPackedSize(format)
+end
 
 ---Compute the message digest of a string using a specified hash algorithm.
 ---@param hashFunction HashFunction @Hash algorithm to use.
 ---@param string string @String to hash.
 ---@return string
 ---@overload fun(hashFunction:HashFunction, data:Data):string
-function m.hash(hashFunction, string) end
+function m.hash(hashFunction, string)
+end
 
 ---Creates a new Data object containing arbitrary bytes.
 ---
@@ -115,14 +122,16 @@ function m.hash(hashFunction, string) end
 ---@return ByteData
 ---@overload fun(Data:Data, offset:number, size:number):ByteData
 ---@overload fun(size:number):ByteData
-function m.newByteData(datastring) end
+function m.newByteData(datastring)
+end
 
 ---Creates a new Data referencing a subsection of an existing Data object.
 ---@param data Data @The Data object to reference.
 ---@param offset number @The offset of the subsection to reference, in bytes.
 ---@param size number @The size in bytes of the subsection to reference.
 ---@return Data
-function m.newDataView(data, offset, size) end
+function m.newDataView(data, offset, size)
+end
 
 ---Packs (serializes) simple Lua values.
 ---
@@ -132,7 +141,8 @@ function m.newDataView(data, offset, size) end
 ---@param v1 number or boolean or string @The first value (number, boolean, or string) to serialize.
 ---@param ... number or boolean or string @Additional values to serialize.
 ---@return Data or string
-function m.pack(container, format, v1, ...) end
+function m.pack(container, format, v1, ...)
+end
 
 ---Unpacks (deserializes) a byte-string or Data into simple Lua values.
 ---
@@ -142,6 +152,7 @@ function m.pack(container, format, v1, ...) end
 ---@param pos number @Where to start reading in the string. Negative values can be used to read relative from the end of the string.
 ---@return number or boolean or string, number or boolean or string, number
 ---@overload fun(format:string, data:Data, pos:number):number or boolean or string, number or boolean or string, number
-function m.unpack(format, datastring, pos) end
+function m.unpack(format, datastring, pos)
+end
 
 return m
