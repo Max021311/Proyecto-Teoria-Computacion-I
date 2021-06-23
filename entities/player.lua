c1319990d4213e6e425acd3ca4c94ab737bc0f26
Jose Maximiliano Diaz Mendez <josemaximilianodiazmendez@gmail.com>
2021-06-23 00:00:45 -0500

    Player now have health and destroy animation

--- entities/player.lua
+++ entities/player.lua
@@ -11,11 +11,12 @@ local Player =
 function Player:init(x, y)
   Entity.init(self, x, y)
   self.type = 'player'
-  self.lives = 3
+  self.health = 10
   self.speed = 225
   self.width = 32
   self.height = 32
   self.invincible = false
+  self.removing = false
 end
 
 function Player:registerCollision(collider)
@@ -28,10 +29,35 @@ function Player:draw()
   if self.hitbox and Debug then
     self.hitbox:draw()
   end
-  love.graphics.circle('line', self.position.x + 16, self.position.y + 16, 16)
+  love.graphics.circle('line', self.position.x + self.width / 2, self.position.y + self.height / 2, self.width / 2)
 end
 
 function Player:update(dt)
+  if love.keyboard.isDown('escape') or self.health <= 0 then
+    self.health = 0
+    if not self.removing then
+      Timer.script(
+        function(wait)
+          self.removing = true
+          wait(1)
+          self.position.x = self.position.x + self.width / 4
+          self.position.y = self.position.y + self.height / 4
+          self.width = self.width / 2
+          self.height = self.height / 2
+          wait(1)
+          self.position.x = self.position.x - (self.width / 2)
+          self.position.y = self.position.y - (self.height / 2)
+          self.width = self.width * 2
+          self.height = self.height * 2
+          wait(1)
+          self:destroy()
+          self.manager.player = nil
+        end
+      )
+    end
+    return
+  end
+
   local desiredDirection = Vector(0, 0)
 
   if love.keyboard.isDown('w', 'up') then
@@ -52,7 +78,7 @@ end
 
 function Player:onCollide(otherEntity)
   if otherEntity.type == 'attack' and not self.invincible then
-    Hits = Hits + 1
+    self.health = self.health - otherEntity.damage
     self.invincible = true
     Timer.after(
       2,
